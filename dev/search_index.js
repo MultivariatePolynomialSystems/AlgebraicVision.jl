7be2899226d92e5c7582686ca9db6a4ce32419e4
Documenter.jl <documenter@juliadocs.github.io>
2023-10-22 08:34:05 +0000

    build based on a8f8693

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AlgebraicVision","category":"page"},{"location":"#AlgebraicVision","page":"Home","title":"AlgebraicVision","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AlgebraicVision.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AlgebraicVision]","category":"page"}]
+}
